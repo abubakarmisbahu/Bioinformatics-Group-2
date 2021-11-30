bc46cd8b109588120500bc165c939465aa374c87
Addah35 <95129770+Addah35@users.noreply.github.com>
2021-11-30 21:09:33 +0100

    Add files via upload

---
+++ Script_Amina.py
@@ -0,0 +1,8 @@
+#<-----------Amina Project------------>
+print("\t-------------BIOINFORMATICS SCRIPT---------------\n")
+
+print("\t\tHello Amina Hussaini Badaromo, below are your informations:\n")
+print("Name: " + "\t\t\t" + "Amina Hussaini Badaromo")
+print("Email: " + "\t\t\t"+ "aminabadaromo35@gmail.com")
+print("Slack_username: " + "\t" + "@Amina_H_Badaromo")
+print("Area of interest: " + "\t" + "Genomics")
