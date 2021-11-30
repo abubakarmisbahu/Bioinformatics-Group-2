5d97bcbfe8c3173fec2650ea2cdde2597df47d4f
Aishatu11 <94998808+Aishatu11@users.noreply.github.com>
2021-11-30 22:40:16 +0100

    Add files via upload

---
+++ script_aisha.R
@@ -0,0 +1,20 @@
+getwd
+
+# FULL NAME
+name<- c('aishatu musa muhammad')
+name
+
+# EMAIL ADDRESS
+email<- c('marwaje23@gmail.com')
+email
+
+# SLACK USERNAME
+slack<- c('aishatu.m.muhammad')
+slack
+
+# BIOINFORMATIC AREA OF INTEREST
+bioinformatic<- c('system biology')
+bioinformatic
+
+rscript<- data.frame(name, email, slack, bioinformatic)
+rscript
