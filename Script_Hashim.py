df2fef21c38655607995f074ef952710de9efe45
Hashimhbala <95073966+Hashimhbala@users.noreply.github.com>
2021-12-02 00:46:47 +0100

    Script_Hashim.py

---
+++ Script_Hashim.py
@@ -0,0 +1,8 @@
+#<-----------Hashim Project------------>
+print("\t-------------BIOINFORMATICS SCRIPT---------------\n")
+
+print("\t\tHello Hashim H Bala, below are your informations:\n")
+print("Name: " + "\t\t\t" + "Hashim H Bala")
+print("Email: " + "\t\t\t"+ "bhashimh@gmail.com")
+print("Slack_username: " + "\t" + "@HASHIM_H_BALA")
+print("Area of interest: " + "\t" + "Proteomics")
