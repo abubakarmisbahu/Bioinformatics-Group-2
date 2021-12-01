ac64d1f784fc56dfc055319b09b653f3cdefeddc
Ustuu02 <95091113+Ustuu02@users.noreply.github.com>
2021-12-01 16:48:27 +0100

    Add files via upload

---
+++ script ustuu2020.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "-------------------BIOINFORMATICS C++ SCRIPT----------------\n";
+    cout << "\t\tHello usman Muhammad:\n\n";
+    cout << "Name:"<< "\t\t\t" << "usman muhammad\n";
+    cout << "Email:"<< "\t\t\t" << "ustuu2020@gmail.com\n";
+    cout << "Slack_Id:" << "\t\t" << "@usman_muhammad\n";
+    cout << "Area Of Interest:" << "\t" << "epigenomics\n";
+    return 0;
+}
