d6884f17fbf6abe9912e2cb5790bc573be96b78c
lukmanmahmudyahaya <95088163+lukmanmahmudyahaya@users.noreply.github.com>
2021-12-01 17:07:40 +0100

    Add files via upload

---
+++ lukmanmahmudyahaya.java
@@ -0,0 +1,16 @@
+public class MyClass {
+    public static void main(String args[]) {
+     /*
+        A JAVA PROGRAME TO PRINT OUT MY DETAILS
+        WRITTEN BY:
+            LUKMAN MAHMUD YAHAYA
+            BASUG/UG/BHS/ANA/ANT/18/017
+     */
+
+      System.out.println("NAME: LUKMAN MAHMUD YAHAYA");
+      System.out.println("EMAIL: lukmanmahmudyahaya18@gmail.com ");
+      System.out.println("SLACK USERNAME: @Lukman Mahmud Yahaya");
+      System.out.println("GITHUB: lukmanmahmudyahaya");
+      System.out.println("AREA OF INTEREST: GENETICS");
+    }
+}
