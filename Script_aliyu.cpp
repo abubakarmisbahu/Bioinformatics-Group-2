248fff28d8929557da79c836ac602a2b467dfae9
Kotso033 <95000451+Kotso033@users.noreply.github.com>
2021-12-01 16:44:50 +0100

    Add files via upload

---
+++ Script_aliyu.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "-------------------BIOINFORMATICS C++ SCRIPT----------------\n";
+    cout << "\t\tHello Aliyu Muhammad:\n\n";
+    cout << "Name:"<< "\t\t\t" << "Aliyu Muhammad\n";
+    cout << "Email:"<< "\t\t\t" << "aliyuhammad1@gmail.com\n";
+    cout << "Slack_Id:" << "\t\t" << "@Aliyu_Muhammad\n";
+    cout << "Area Of Interest:" << "\t" << "Genomics\n";
+    return 0;
+}
