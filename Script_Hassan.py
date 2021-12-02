b6b3e2c844318538aa0ddccd773e7ce27e3137db
Abubakar Misbahu <95080134+abubakarmisbahu@users.noreply.github.com>
2021-12-02 19:06:20 +0100

    Add files via upload

---
+++ Script_Hassan.py
@@ -0,0 +1,8 @@
+#<-----------Suraja Project------------>
+print("\t-------------BIOINFORMATICS SCRIPT---------------\n")
+
+print("\t\tHello Hassan Muhammad Aliyu, below are your informations:\n")
+print("Name: " + "\t\t\t" + "Hassan Muhammad Aliyu")
+print("Email: " + "\t\t\t"+ "hassanmuhammadaliyou@gmail.com")
+print("Slack_username: " + "\t" + "@Hassan_M_Aliyu")
+print("Area of interest: " + "\t" + "Genomics")
