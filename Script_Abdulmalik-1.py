f2885c0a71b8067c48675e54a1c1e26d5d553783
Abubakar Misbahu <95080134+abubakarmisbahu@users.noreply.github.com>
2021-12-02 12:54:56 +0100

    Add files via upload

---
+++ Script_Abdulmalik-1.py
@@ -0,0 +1,8 @@
+#<-----------Abdulmalik Project------------>
+print("\t-------------BIOINFORMATICS SCRIPT---------------\n")
+
+print("\t\tHello Abdulmalik Sunusi, below are your informations:\n")
+print("Name: " + "\t\t\t" + "Abdulmalik Sunusi")
+print("Email: " + "\t\t\t"+ "abdulmaliksanusi2000@gmail.com")
+print("Slack_username: " + "\t" + "@Abdumalik_Sunusi")
+print("Area of interest: " + "\t" + "Proteomics")
