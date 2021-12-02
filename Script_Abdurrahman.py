3c6c306c4b16ba2ff3674a7efc11252f94984f2b
Abubakar Misbahu <95080134+abubakarmisbahu@users.noreply.github.com>
2021-12-02 12:56:48 +0100

    Add files via upload

---
+++ Script_Abdurrahman.py
@@ -0,0 +1,8 @@
+#<-----------Abdurrahman Project------------>
+print("\t-------------BIOINFORMATICS SCRIPT---------------\n")
+
+print("\t\tHello Abdurrahman Abubakar Abdullahi, below are your informations:\n")
+print("Name: " + "\t\t\t" + "Abdurrahman Abubakar Abdullahi")
+print("Email: " + "\t\t\t"+ "abdurrahmanitas2017@gmail.com")
+print("Slack_username: " + "\t" + "@Abdurrahman_A_Abdullahi")
+print("Area of interest: " + "\t" + "Genomics")
